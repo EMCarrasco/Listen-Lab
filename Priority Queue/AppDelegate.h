f153bbb3701d31458df059f15d2b11dee43ce749
Abelardo Torres <Vicky@Abelardos-Mac-mini.local>
2015-06-13 13:38:07 -0500

    Initial Commit

---
+++ Priority Queue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Priority Queue
+//
+//  Created by Abelardo Torres on 6/13/15.
+//  Copyright (c) 2015 AE Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Priority Queue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Priority Queue
+//
+//  Created by Abelardo Torres on 6/13/15.
+//  Copyright (c) 2015 AE Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
